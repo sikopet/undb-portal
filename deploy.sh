be4b40bf0bd98a81844ce9dba8d681a3274db471
Blaise Fonseca <blaisefonseca@hotmail.com>
2015-12-16 15:38:39 -0500

    correct docker image name

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build -t localhost:5000/undb-cbd-int git@github.com:scbd/undb.cbd.int
+docker build -t localhost:5000/undb-portal git@github.com:scbd/undb.cbd.int
 
-docker push localhost:5000/undb-cbd-int
+docker push localhost:5000/undb-portal
