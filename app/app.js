d550d3775fe51ac1dee85c18a8a3ca59dc6cf169
Randy Houlahan <randy.houlahan@cbd.int>
2016-01-12 19:44:41 -0500

    realm config

--- app/app.js
+++ app/app.js
@@ -6,12 +6,35 @@ define(['angular'], function(angular) { 'use strict';
 
     var app = angular.module('app', deps);
 
-    app.config(['$httpProvider', function($httpProvider){
+    app.config(['$httpProvider','$provide', function($httpProvider,$provide){
 
         $httpProvider.useApplyAsync(true);
         $httpProvider.interceptors.push('authenticationHttpIntercepter');
+        $httpProvider.interceptors.push('realmHttpIntercepter');
+
 
     }]);
+
+    app.factory('realmHttpIntercepter', ["realm", function(realm) {
+
+		return {
+			request: function(config) {
+				var trusted = /^https:\/\/api.cbd.int\//i .test(config.url) ||
+						      /^https:\/\/localhost[:\/]/i.test(config.url) ||
+							  /^\/\w+/i                   .test(config.url);
+
+                //exception if the APi call needs to be done for different realm
+                if(trusted && realm && config.params && config.params.realm && config.params.realm != realm) {
+                      config.headers = angular.extend(config.headers || {}, { realm : config.params.realm });
+                }
+                else if(trusted && realm ) {
+                    config.headers = angular.extend(config.headers || {}, { realm : realm });
+                }
+
+                return config;
+			}
+		};
+	}]);
 
     return app;
 });
