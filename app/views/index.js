9d2aa21caac7e44072766ee0d1c7b77af0d4fd85
Vince Gopez <vince.gopez@cbd.int>
2016-01-14 09:37:58 -0500

    Home page updates (WFP). Deleted unused images.

--- app/views/index.js
+++ app/views/index.js
@@ -5,5 +5,43 @@ define(['app', 'angular', 'authentication'], function() { 'use strict';
 
 
 
+		$scope.carouselData=  {
+					items :[{
+						title : "2015 is the Time for Global Action",
+						description : "2015 presents a historic and unprecedented opportunity to bring the countries and citizens of the world together to decide and embark on new paths to improve the lives of people everywhere",
+						imageUrl : "app/images/carousel/slider-1.jpg",
+						targetUrl: "http://www.cbd.int"
+					},
+					{
+						title : "2015 is the Time for Global Action",
+						description : "2015 presents a historic and unprecedented opportunity to bring the countries and citizens of the world together to decide and embark on new paths to improve the lives of people everywhere",
+						imageUrl : "http://lorempixel.com/865/467/nature/two/",
+						targetUrl: "http://www.cbd.int"
+					},
+					{
+						title : "Title 3",
+						description : "Description 3 Description 3 Description 3 Description 3 Description 3",
+						imageUrl : "http://lorempixel.com/865/467/nature/three/",
+						targetUrl: "http://www.cbd.int"
+					},
+					{
+						title : "Title 4",
+						description : "Description 4",
+						imageUrl : "http://lorempixel.com/865/467/nature/four/",
+						targetUrl: "http://www.cbd.int"
+					},
+					{
+						title : "Title 5",
+						description : "Description 5",
+						imageUrl : "http://lorempixel.com/865/467/nature/five/",
+						targetUrl: "http://www.cbd.int"
+					},
+					{
+						title : "Title 6",
+						description : "Description 6",
+						imageUrl : "http://lorempixel.com/865/467/nature/six/",
+						targetUrl: "http://www.cbd.int"
+					}]
+				};
     }];
 });
