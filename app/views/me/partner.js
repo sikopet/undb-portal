9290bf309871dec133805a70d271bffc1d423447
Stephane Bilodeau <bilodeaustephane@gmail.com>
2016-01-14 16:22:33 -0500

    Load & Save record

--- app/views/me/partner.js
+++ app/views/me/partner.js
@@ -1,11 +1,12 @@
 define(['lodash', 'guid', 'app'], function(_, guid) { 'use strict';
 
-    return ['$scope', '$http', 'locale', function($scope, $http, locale) {
+    return ['$scope', '$http', '$q', 'locale', function($scope, $http, $q, locale) {
 
-        $scope.submit           = submit;
+        $scope.save           = save;
         $scope.googleMapsChange = updateGeoLocation;
 
-        $http.get('https://api.cbd.int/api/v2015/countries', { cache:true, params: { f : { code : 1, name : 1 } } }).then(function(res) {
+        // Init
+        $http.get('/api/v2015/countries', { cache:true, params: { f : { code : 1, name : 1 } } }).then(function(res) {
 
             res.data.forEach(function(c) {
                 c.code = c.code.toLowerCase();
@@ -14,6 +15,8 @@ define(['lodash', 'guid', 'app'], function(_, guid) { 'use strict';
 
             $scope.countries = res.data;
         });
+
+        load();
 
         //==============================
         //
@@ -38,17 +41,95 @@ define(['lodash', 'guid', 'app'], function(_, guid) { 'use strict';
         //
         //
         //==============================
-        function submit(doc) {
+        function load() {
 
-            if(!doc.header) {
-                doc.header = {
-                    identifier : guid(),
-                    schema : 'undbPartner'
-                };
-            }
+            $scope.loading = true;
 
-            alert("TODO:"+doc);
+            var filter = "type eq 'undbPartner'";
+
+            var resDrafts = $http.get("/api/v2013/documents", { params : { collection : 'mydraft', $filter : filter } });
+            var resDocs   = $http.get("/api/v2013/documents", { params : { $filter : filter } });
+
+            $q.all([resDrafts, resDocs]).then(function(res) {
+
+                var records = _.flatten([res[0].data.Items, res[1].data.Items]);
+
+                if(records.length)
+                    return records[0];
+
+            }).then(function(record){
+
+                if(!record)
+                    return;
+
+                if(record.workingDocumentID) return $http.get('/api/v2013/documents/'+record.identifier+'/versions/draft');
+                else                         return $http.get('/api/v2013/documents/'+record.identifier);
+
+            }).then(function(res) {
+
+                if(res) {
+                    $scope.document = res.data;
+                }
+                else {
+                    $scope.document = {
+                        header : {
+                            identifier : guid(),
+                            schema : 'undbPartner'
+                        }
+                    };
+                }
+
+            }).catch(function(err) {
+
+                err = (err||{}).data || err;
+
+                console.error(err);
+
+            }).finally(function(){
+
+                delete $scope.loading;
+
+            });
         }
 
+        //==============================
+        //
+        //
+        //==============================
+        function save() {
+
+            $scope.saving = true;
+
+            delete $scope.errors;
+
+            var doc = $scope.document;
+
+            return $http.put('/api/v2013/documents/validate', doc, { params : { schema : doc.header.schema }}).then(function(res) {
+
+                var report = res.data;
+
+                if(report.errors && report.errors.length)
+                    throw report;
+
+                return $http.put('/api/v2013/documents/'+doc.header.identifier+'/versions/draft', doc, { params : { schema : doc.header.schema }});
+
+            }).then(function(res) {
+
+                console.log(res.data || res);
+
+            }).catch(function(err) {
+
+                err = err.data || err;
+
+                $scope.errors = err.errors || [err];
+
+                console.error(err);
+
+            }).finally(function(){
+
+                delete $scope.saving;
+
+            });
+        }
     }];
 });
