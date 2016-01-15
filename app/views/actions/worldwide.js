6a482092b68bc931896c5f6a19b58e09848955bc
Randy Houlahan <randy.houlahan@cbd.int>
2016-01-15 11:11:07 -0500

    resolve conflict

--- app/views/actions/worldwide.js
+++ app/views/actions/worldwide.js
@@ -1,28 +1,12 @@
-define(['jquery','app', 'angular', 'authentication', '../../directives/map/undb-map'], function($) { 'use strict';
+
+define(['app', 'angular', 'authentication', '../../directives/map/undb-map'], function() { 'use strict';
 
 	return ['$scope', '$rootScope', '$route', '$browser', '$location', '$window', 'authentication',
     function ($scope, $rootScope, $route, $browser, $location, $window, authentication) {
 
-			/* MAP */
-
-			// closing the green popup
-			// $('#map .mapPopup button.closeButton').click(function(){
-			// 	$(this).parent('.mapPopup').hide();
-			// });
-
-			// // selecting a UNDB Network
-			// $('.sideSelectionRow').click(function(){
-			// 	$('.sideSelectionRowActive').addClass('sideSelectionRow');
-			// 	$('.sideSelectionRowActive').removeClass('sideSelectionRowActive');
-			//
-			// 	$(this).addClass('sideSelectionRowActive');
-			//
-			// 	var text = $(this).data('text');
-			// 	$('.mapPopup').children('p').html(text)
-			// 	$('.mapPopup').show();
-			// });
-
-			/* ////MAP */
+		$scope.actionRegister = function () {
+			$window.location.href = '/actions/submit';
+		}
 
     }];
 });
