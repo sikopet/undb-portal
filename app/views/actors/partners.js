46f643f76f45b430c5ac48800d21905b45153843
Stephane Bilodeau <bilodeaustephane@gmail.com>
2016-01-20 16:21:01 -0500

    Load Partners from index (stub)

--- app/views/actors/partners.js
+++ app/views/actors/partners.js
@@ -1,8 +1,23 @@
 define(['app', 'angular', 'authentication'], function() { 'use strict';
 
-	return ['$scope', function ($scope) {
+	return ['$scope', '$http','user', function ($scope, $http, user) {
 
-        //TODO CHECK USER ALREADY A PARTNER
+        var qsParams =
+        {
+            "q"  : 'schema_s:undbPartner AND _state_s:public',
+            "fl" : "identifier_s, title_t, description_t",
+            "sort"  : "updatedDate_dt desc",
+            "start" : 0,
+            "row"   : 1000,
+        };
 
+        $http.get('https://api.cbd.int/api/v2013/index', { params : qsParams }).then(function(res) {
+            $scope.partners = res.data.response.docs;
+        });
+
+
+        if(user.isAuthenticated) {
+            //TODO CHECK USER ALREADY A PARTNER and disbale subscribe button
+        }
     }];
 });
