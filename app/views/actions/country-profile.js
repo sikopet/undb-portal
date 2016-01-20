1f670a90a70fe34a6027c0f475ad0a99f4592c63
Randy Houlahan <randy.houlahan@cbd.int>
2016-01-20 13:20:53 -0500

    actions and partners on profile page

--- app/views/actions/country-profile.js
+++ app/views/actions/country-profile.js
@@ -19,9 +19,48 @@ define(['app', '../../directives/map/zoom-map'], function() { 'use strict';
 
 			$scope.country = res.data;
 			console.log($scope.country);
-		});
+		}).then(function(){
 
-console.log('$location.search();',$scope.code);
+			var queryParameters = {
+				'q': 'schema_s:undbPartner AND _state_s:public AND country_s:'+$scope.country.code,
+				//  'sort': 'createdDate_dt desc, title_t asc',
+				'wt': 'json',
+				'start': 0,
+				'rows': 1000000,
+			};
+				$http.get('/api/v2013/index/select', {
+					params: queryParameters,
+					cache: true
+				}).success(function(data) {
+					$scope.count = data.response.numFound;
+					$scope.partners = data.response.docs;
+console.log($scope.partners);
+				});
+				queryParameters = {
+					'q': 'schema_s:undbAction  AND country_s:'+$scope.country.code, //AND _state_s:public removed for test
+					//  'sort': 'createdDate_dt desc, title_t asc',
+					'wt': 'json',
+					'start': 0,
+					'rows': 1000000,
+				};
+					$http.get('/api/v2013/index/select', {
+						params: queryParameters,
+						cache: true
+					}).success(function(data) {
+						$scope.count = data.response.numFound;
+						$scope.actions = data.response.docs;
+						$scope.actions.forEach(function(a) {
+								var dateObject = new Date(Date.parse(a.startDate_s));
+								a.startDate_s = dateObject.toDateString();
+								var dateObject2 = new Date(Date.parse(a.endDate_s));
+								a.endDate_s=dateObject2.toDateString();
+						});
+
+
+
+console.log($scope.actions);
+					});
+		});
 
 		//=======================================================================
 		//
@@ -30,12 +69,6 @@ console.log('$location.search();',$scope.code);
 			$location.url('/actions/countries/'+code.toUpperCase());
 		};
 
-		//=======================================================================
-		//
-		//=======================================================================
-		$scope.actionRegister = function () {
-			$location.url('/actions/submit');
-		};
 
     }];
 });
