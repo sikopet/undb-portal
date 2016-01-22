13950b2677bd9e5d4bcb3bb3e87903e9244eacf9
Stephane Bilodeau <bilodeaustephane@gmail.com>
2016-01-22 10:20:40 -0500

    Move repository to `undb-portal`

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build -t localhost:5000/undb-portal git@github.com:scbd/undb.cbd.int
+docker build -t localhost:5000/undb-portal git@github.com:scbd/undb-portal
 
 docker push localhost:5000/undb-portal
