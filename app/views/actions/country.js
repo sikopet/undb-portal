b6260fffc09aea849705965a183719b9eb31a0ad
Randy Houlahan <randy.houlahan@cbd.int>
2016-06-19 12:31:59 -0400

    show profiles only with events with facets as per https://trello.com/c/QkaXHZIY

--- app/views/actions/country.js
+++ app/views/actions/country.js
@@ -1,4 +1,4 @@
-define(['app'], function() { 'use strict';
+define(['app','lodash'], function(app,_) { 'use strict';
 
 	return ['$scope','locale','$http', '$location',
     function ($scope,locale, $http, $location) {
@@ -21,7 +21,32 @@ define(['app'], function() { 'use strict';
 				c.name = c.name[locale];
 				c.cssClass='flag-icon-'+c.code;
 			});
-			$scope.countries = res.data;
+			$http.get("https://api.cbd.int/api/v2013/index", {
+					 params: {
+							 'q': 'schema_s:undbAction',
+							 'sort': 'createdDate_dt desc, title_t asc',
+							 'wt': 'json',
+							 'start': 0,
+							 'rows': 1000000,
+							 'facet':true,
+							 'facet.field':'country_s'
+					 }
+	 		}).then(function(resData){
+					var country,index;
+					$scope.countries=[];
+					_.each(resData.data.response.docs,function(action){
+
+								country = _.find(res.data,{code:action.country_s});
+								if(!country) throw "No country found forcode: " + action.country_s;
+								else {
+									if(!_.find($scope.countries,{code:action.country_s})){
+										index=resData.data.facet_counts.facet_fields.country_s.indexOf(action.country_s)+1;
+										country.facet=resData.data.facet_counts.facet_fields.country_s[index];
+										$scope.countries.push(country);
+									}
+								}
+					});
+			});
 		});
 
 		//=======================================================================
