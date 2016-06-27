ee52d31466087de56a852ccb6d67a086fd731ab9
Randy Houlahan <randy.houlahan@cbd.int>
2016-06-27 10:15:53 -0400

    config angular toast, could not dynamically load angular-text so loaded in app.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
-define(['angular'], function(angular) { 'use strict';
+define(['angular','text-angular-setup','text-angular','text-angular-sanitize'], function(angular) { 'use strict';
 
-    var deps = ['ngRoute'];
+    var deps = ['ngRoute','smoothScroll','textAngularSetup','ngSanitize','textAngular','toastr'];
 
     angular.defineModules(deps);
 
@@ -14,6 +14,52 @@ define(['angular'], function(angular) { 'use strict';
         $httpProvider.interceptors.push('authenticationHttpIntercepter');
         $httpProvider.interceptors.push('realmHttpIntercepter');
     }]);
+    app.config(['$provide', function($provide){
+  	         // this demonstrates how to register a new tool and add it to the default toolbar
+  	         $provide.decorator('taOptions', ['$delegate', function(taOptions){
+  	             // $delegate is the taOptions we are decorating
+  	             // here we override the default toolbars and classes specified in taOptions.
+  	             taOptions.forceTextAngularSanitize = true; // set false to allow the textAngular-sanitize provider to be replaced
+  	             taOptions.keyMappings = []; // allow customizable keyMappings for specialized key boards or languages
+  	             taOptions.toolbar = [
+  	                 ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
+  	                 ['bold', 'italics', 'underline', 'ul', 'ol', 'redo', 'undo', 'clear'],
+  	                 ['justifyLeft','justifyCenter','justifyRight', 'justifyFull'],
+  	                 ['html', 'insertImage', 'insertLink', 'wordcount', 'charcount']
+  	             ];
+  	             taOptions.classes = {
+  	                 focussed: 'focussed',
+  	                 toolbar: 'btn-toolbar',
+  	                 toolbarGroup: 'btn-group',
+  	                 toolbarButton: 'btn btn-default',
+  	                 toolbarButtonActive: 'active',
+  	                 disabled: 'disabled',
+  	                 textEditor: 'form-control',
+  	                 htmlEditor: 'form-control'
+  	             };
+  	             return taOptions; // whatever you return will be the taOptions
+  	         }]);
+  }]);
+
+  app.config(['toastrConfig',function(toastrConfig) {
+    angular.extend(toastrConfig, {
+      autoDismiss: true,
+      containerId: 'toast-container',
+      newestOnTop: true,
+      closeButton: true,
+      positionClass: 'toast-top-right',
+      iconClasses: {
+        error: 'toast-error',
+        info: 'toast-info',
+        success: 'toast-success',
+        warning: 'toast-warning'
+      },
+      target: 'body',
+      timeOut: 5000,
+      progressBar: true,
+    });
+
+  }]);
 
     app.factory('realmHttpIntercepter', ["realm", function(realm) {
 
