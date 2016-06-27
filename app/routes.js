47f5e7467cbbcde3abe392a35592048ec8312f2d
Randy Houlahan <randy.houlahan@cbd.int>
2016-06-27 10:21:50 -0400

    calendar and action route

--- app/routes.js
+++ app/routes.js
@@ -22,12 +22,13 @@ define(['app', 'lodash', 'text!views/index.html', 'views/index', 'providers/exte
             when('/actions/countries/:code',      { templateUrl: 'views/actions/country-profile.html',  label :'Profile',             resolveController: true }).
             when('/actions/countries/edit/:code', { templateUrl: 'views/actions/country-profile-form.html', label:'Edit Party Profile',   resolveController: true, resolve : { user : securize(["UNDBPublishingAuthority", "undb-administrator"]) } }).
            // when('/actions/un',                 { templateUrl: 'views/actions/un.html',               label:'By UN Organization',   resolveController: true }).
-            when('/actions/calendar',             { templateUrl: 'views/actions/calendar.html',         label:'Calendar'              }).
+            when('/actions/calendar',             { templateUrl: 'views/actions/calendar.html',         label:'Calendar',             resolveController: true }).
             when('/actions/participate',          { templateUrl: 'views/actions/participate.html',      label:'Participate'           }).
             when('/actions/submit',               { templateUrl: 'views/actions/submit.html',           label:'Participate',          resolveController: true, resolve : { user : securize(['User']) }, reloadOnSearch : false }).
             when('/actions/submit-form/:uid?',    { templateUrl: 'views/actions/submit-form.html',      label:'Form',                 resolveController: true, resolve : { user : securize(['User']) } }).
             when('/actions/submit-form-done',     { templateUrl: 'views/actions/submit-form-done.html', label:'Thanks',                                        resolve : { user : securize(['User']) } }).
-
+            when('/actions/:identifier',          { templateUrl: 'views/actions/action.html',           label:'Action',               resolveController: true }).
+            
             when('/actors',                      { templateUrl: 'views/actors/index.html',       label:'Actors'                   }).
             when('/actors/abttf',                { templateUrl: 'views/actors/abttf.html',       label:'ABTTF'                    }).
             when('/actors/champions',            { templateUrl: 'views/actors/champions.html',   label:'Biodiversity Champions'   }).
