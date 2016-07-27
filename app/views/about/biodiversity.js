2e3ab558400a9380412cc86eeace9618c446a9d3
Randy Houlahan <randy.houlahan@cbd.int>
2016-07-27 12:09:18 -0400

    goto function and cleaning

--- app/views/about/biodiversity.js
+++ app/views/about/biodiversity.js
@@ -1,9 +1,9 @@
 define(['app'], function() { 'use strict';
 
-	return ['$scope', function ($scope) {
-		var panels = {"firstCollapse": true};
-        $scope.toggleCss = function (elementID, targetElementID) {
+	return ['$scope','$location', function ($scope,$location) {
+		var panels = {"firstCollapse": false};
 
+        $scope.toggleCss = function (elementID, targetElementID) {
             $('#accordion .panel-heading').removeClass('bg-tall');
             $('#accordion .panel-heading').addClass('bg-short');
 
@@ -12,13 +12,16 @@ define(['app'], function() { 'use strict';
             if(!$('#'+elementID).hasClass('in')){
                 $('#'+targetElementID).removeClass('bg-short');
                 $('#'+targetElementID).addClass('bg-tall');
-                panels[elementID] = true;
             }
         };
 
         $scope.isOpen = function (elementID) {
             return panels[elementID] || false;
         };
+
+				$scope.goTo = function (uri) {
+						$location.url(uri);
+				};
 
     }];
 });
