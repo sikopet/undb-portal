ebee1ad56e9a56a43b4752e0b6d79d2548306fba
Randy Houlahan <randy.houlahan@cbd.int>
2016-09-13 21:43:14 -0400

    fix linker

--- app/filters/uri-to-link.js
+++ app/filters/uri-to-link.js
@@ -1,20 +1,20 @@
 define(['app','lodash'], function(app,_) {
     'use strict';
     app.filter("uriToLink", function() {
-      var  //URLs starting with http://, https://, or ftp://
-     replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim,
+      var  
      //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
      replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim,
      //Change email addresses to mailto:: links.
      replacePattern3 = /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})/gim;
 
      return function(text) {
-         _.each(_.uniq(text.match(replacePattern1)), function(item) {
-           text = replaceAll(text,item,"<a href=\"http://"+item+"\" target=\"_blank\">"+item+"</a>");
-         });
+
+        text = replaceAll(text,'https://',' ');
+        text = replaceAll(text,'http://',' ');
 
          _.each(_.uniq(text.match(replacePattern2)), function(item) {
-            text = replaceAll(text,item,"<a href=\"http://"+item+"\" target=\"_blank\">"+item+"</a>");
+
+            text = replaceAll(text,item.trim(),"<a href=\"http://"+item.trim()+"\" target=\"_blank\">"+item+"</a>");
          });
 
          _.each(_.uniq(text.match(replacePattern3)), function(item) {
