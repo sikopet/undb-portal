ca34d99d7b40a47786aecb03eff695c76592aa41
Randy Houlahan <randy.houlahan@cbd.int>
2016-09-15 08:20:49 -0400

    fix to link maker

--- app/filters/uri-to-link.js
+++ app/filters/uri-to-link.js
@@ -1,21 +1,22 @@
 define(['app','lodash'], function(app,_) {
     'use strict';
     app.filter("uriToLink", function() {
-      var  
+      var
      //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-     replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim,
+     replacePattern2 = /(^|[^\/\>])(\swww\.[\S]+(\b|$))($|[^\"])/gim,
      //Change email addresses to mailto:: links.
-     replacePattern3 = /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})/gim;
+     replacePattern3 = /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})(^|[^\"])/gim;
 
      return function(text) {
 
-        text = replaceAll(text,'https://',' ');
-        text = replaceAll(text,'http://',' ');
+        text = replaceAll(text,' https://',' ');
+        text = replaceAll(text,' http://',' ');
 
          _.each(_.uniq(text.match(replacePattern2)), function(item) {
-
-            text = replaceAll(text,item.trim(),"<a href=\"http://"+item.trim()+"\" target=\"_blank\">"+item+"</a>");
+            item=item.substring(1);
+            text = replaceAll(text,item.trim(),"<a href=\"http://"+item.trim()+"\" target=\"_blank\">"+item.trim()+"</a>");
          });
+
 
          _.each(_.uniq(text.match(replacePattern3)), function(item) {
             text = text.replace(item, '<a href="mailto:'+item+'">'+item+'</a>');
