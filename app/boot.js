779c7fccc2f0aa70c57e8207eb2c066c60ec8e79
Randy Houlahan <randy.houlahan@cbd.int>
2017-04-03 09:41:31 -0400

    new date picker

--- app/boot.js
+++ app/boot.js
@@ -3,43 +3,44 @@ require.config({
     waitSeconds: 120,
     baseUrl : 'app/',
     paths: {
-        'angular'               : 'libs/angular-flex/angular-flex',
-        'angular-route'         : 'libs/angular-route/angular-route',
-        'angular-sanitize'      : 'libs/angular-sanitize/angular-sanitize.min',
-        'authentication'        : 'factories/authentication',
-        'bootstrap'             : 'libs/bootstrap/dist/js/bootstrap',
-        'bootstrap-datepicker'  : 'libs/bootstrap-datepicker/dist/js/bootstrap-datepicker.min',
-        'css'                   : 'libs/require-css/css.min',
-        'lodash'                : 'libs/lodash/lodash',
-        'linqjs'                : 'libs/linqjs/linq.min',
-        'jquery'                : 'libs/jquery/dist/jquery',
-        'guid'                  : 'libs/ui-guid-generator/dist/ui-guid-generator.min',
-        'moment'                : 'libs/moment/min/moment.min',
-        'moment-timezone'       : 'libs/moment-timezone/builds/moment-timezone-with-data-2010-2020.min',
-        'ngCookies'             : 'libs/angular-cookies/angular-cookies.min',
-        'ngSmoothScroll'        : 'libs/ngSmoothScroll/lib/angular-smooth-scroll',
-        'shim'                  : 'libs/require-shim/src/shim',
-        'text'                  : 'libs/requirejs-text/text',
-        'toastr'                : 'libs/angular-toastr/dist/angular-toastr.tpls.min',
-        'URIjs'                 : 'libs/uri.js/src',
-        'ng-ckeditor'           : 'libs/ng-ckeditor/ng-ckeditor',
-        'ckeditor'              : 'libs/ng-ckeditor/libs/ckeditor/ckeditor',
-        'ngDialog'              : 'libs/ng-dialog/js/ngDialog.min',
-        'ngInfiniteScroll'      : 'libs/ngInfiniteScroll/build/ng-infinite-scroll'
+        'angular'                   : 'libs/angular-flex/angular-flex',
+        'angular-route'             : 'libs/angular-route/angular-route',
+        'angular-sanitize'          : 'libs/angular-sanitize/angular-sanitize.min',
+        'authentication'            : 'factories/authentication',
+        'bootstrap'                 : 'libs/bootstrap/dist/js/bootstrap',
+        'css'                       : 'libs/require-css/css.min',
+        'lodash'                    : 'libs/lodash/lodash',
+        'linqjs'                    : 'libs/linqjs/linq.min',
+        'jquery'                    : 'libs/jquery/dist/jquery',
+        'guid'                      : 'libs/ui-guid-generator/dist/ui-guid-generator.min',
+        'moment'                    : 'libs/moment/min/moment.min',
+        'moment-timezone'           : 'libs/moment-timezone/builds/moment-timezone-with-data-2010-2020.min',
+        'ngCookies'                 : 'libs/angular-cookies/angular-cookies.min',
+        'ngSmoothScroll'            : 'libs/ngSmoothScroll/lib/angular-smooth-scroll',
+        'shim'                      : 'libs/require-shim/src/shim',
+        'text'                      : 'libs/requirejs-text/text',
+        'toastr'                    : 'libs/angular-toastr/dist/angular-toastr.tpls.min',
+        'URIjs'                     : 'libs/uri.js/src',
+        'ng-ckeditor'               : 'libs/ng-ckeditor/ng-ckeditor',
+        'ckeditor'                  : 'libs/ng-ckeditor/libs/ckeditor/ckeditor',
+        'ngDialog'                  : 'libs/ng-dialog/js/ngDialog.min',
+        'ngInfiniteScroll'          : 'libs/ngInfiniteScroll/build/ng-infinite-scroll',
+'eonasdan-bootstrap-datetimepicker' :'libs/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min'
     },
     shim: {
         'libs/angular/angular'     : { deps: ['jquery'] },
+        'jquery-ui'                : { deps: ['jquery'] },
         'angular'                  : { deps: ['libs/angular/angular'] },
         'angular-route'            : { deps: ['angular'] },
         'ngCookies'                : { deps : ['angular'] },
         'bootstrap'                : { deps: ['jquery'] },
-        'bootstrap-datepicker'     : { deps: ['css!libs/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css'] },
-        'ngSmoothScroll'           : { deps:[ 'angular']},
+        'ngSmoothScroll'           : { deps: [ 'angular']},
         'guid'                     : { exports: 'ui_guid_generator' },
         'toastr'                   : { deps: ['angular']},
         'angular-sanitize'         : { deps: ['angular'] },
         'ng-ckeditor'              : { deps: ['angular','ckeditor']},
-        'ngDialog'                 : { deps : ['angular' ]},
+        'ngDialog'                 : { deps: ['angular' ]},
+        'eonasdan-bootstrap-datetimepicker': { deps: ['jquery','moment','bootstrap','css!libs/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css'] },
     },
     packages: [
         { name: 'ammap', main: 'ammap', location : 'libs/ammap3/ammap' }
@@ -49,7 +50,7 @@ require.config({
 // BOOT
 if (!require.defined('_slaask'))
     define("_slaask", window._slaask);
-    
+
 require(['angular', 'app', 'bootstrap', 'authentication', 'routes', 'template'], function(ng, app) {
 
     ng.element(document).ready(function () {
