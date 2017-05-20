425f4a570700055c0c2378cf9560f5de07d780f1
Randy Houlahan <randy.houlahan@cbd.int>
2017-05-20 18:48:35 -0400

    add realm to events

--- app/views/dashboard/record-list.js
+++ app/views/dashboard/record-list.js
@@ -410,7 +410,7 @@ define(['lodash',
                 // Add Schema
 
                 query.push("schema_s:" + escape(options.schema));
-                // query.push(["realm_ss:" + 'chm-dev', "(*:* NOT realm_ss:*)"]);
+                query.push(["realm_ss:" + realm, "(*:* NOT realm_ss:*)"]);
 
                 // Apply ownership/contributor
 
